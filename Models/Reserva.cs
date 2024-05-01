7582a604c14eaad78ec58f6f5150820726919093
Saullo Santos <saullo.s.r@hotmail.com>
2024-05-01 10:06:49 -0300

    Codigo do desafio aplicado e testado

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade  )
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new ArgumentException("A capacidade da suite é menor que o numero de hospedes, por favor escolha outra suite.");
             }
         }
 
@@ -37,7 +38,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int QuantidadeHospedes = Hospedes.Count;
+            return QuantidadeHospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +47,16 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal TotalDiaria = DiasReservados * Suite.ValorDiaria;
+            decimal valor = TotalDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = TotalDiaria * 0.10M;
+                decimal ToalDiariaComDesconto = TotalDiaria - desconto;
+                valor = ToalDiariaComDesconto;
             }
 
             return valor;
